0e4531316fc5707e029bd56fd563973d2ded3afa
arduqq <twop@hotmail.de>
2019-08-08 18:59:29 +0200

    First Frontend implementation

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <BucketList v-bind:buckets="buckets" v-bind:owner="owner" v-bind:bucketlistnumber="bucketlistnumber"/>
+  </div>
+</template>
+
+<script>
+import BucketList from './components/BucketList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    BucketList
+  },
+  data() {
+    return {
+      owner: "Arduqq",
+      bucketlistnumber: '1863817',
+      buckets: [{
+        title: 'Zu einem Rammstein-Konzert gehen',
+        id: 183628,
+        done: true,
+      }, {
+        title: 'Manila Luzon treffen',
+        id: 192846,
+        done: false,
+      }, {
+        title: 'In Drag eine Performance abliefern',
+        id: 197472,
+        done: false,
+      }],
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/BucketList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="BucketList">
+        <h1>You are currently viewing the bucketlist of {{ owner }} ({{ bucketlistnumber }})</h1>
+        <p>
+            <strong>Progression</strong> - Mopped {{buckets.filter(bucket => {return bucket.done === true}).length}} out of
+            {{buckets.filter(bucket => {return bucket.done === false}).length}} buckets!
+        </p>
+        <ul class="BucketListEntries" v-for="bucket in buckets" :key="bucket.id">
+            <li class="BucketListEntry">{{ bucket.title }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BucketList",
+        props: {
+            buckets: Object,
+            owner: String,
+            bucketlistnumber: Int16Array
+        }
+    }
+</script>
+
+<style scoped>
+    .BucketList {
+        border: solid black 2px;
+        border-radius: 15px;
+        padding: 5%;
+    }
+
+    .BucketListEntries {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+    }
+
+    .BucketListEntry {
+        display: block;
+    }
+</style>
